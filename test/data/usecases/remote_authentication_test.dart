c52a5dfaf545def087690c3c93ffa69b56d61f49
rethink-daniel <daniel.vidal@rethink.dev>
2022-07-01 09:38:08 -0300

    chore: change Auth usecase to receive a model

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -37,9 +37,14 @@ void main() {
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
-  test('Should call HttpClient with correct URL', () async {
+  test('Should call HttpClient with correct values', () async {
     await sut.auth();
 
-    verify(httpClient.request(url: url, method: 'post'));
+    verify(
+      httpClient.request(
+        url: url,
+        method: 'post',
+      )
+    );
   });
 }
