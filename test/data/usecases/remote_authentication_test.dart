0f064e9475db1abf875d3dd316c97ac617cfa8e4
rethink-daniel <daniel.vidal@rethink.dev>
2022-07-04 12:50:11 -0300

    chore: ensure RemoteAuthentication throws UnexpectedErro 400

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,10 +2,11 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:faker/faker.dart';
 
-import 'package:treinamento_flutter/data/usecases/usecases.dart';
+import 'package:treinamento_flutter/domain/helpers/helpers.dart';
 import 'package:treinamento_flutter/domain/usecases/usecases.dart';
-import 'package:treinamento_flutter/data/http/http.dart';
 
+import 'package:treinamento_flutter/data/usecases/usecases.dart';
+import 'package:treinamento_flutter/data/http/http.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -21,7 +22,8 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
-    final params = AuthenticationParams(email: faker.internet.email(), password: faker.internet.password());
+    final params = AuthenticationParams(
+        email: faker.internet.email(), password: faker.internet.password());
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -29,5 +31,19 @@ void main() {
       method: 'post',
       body: {'email': params.email, 'password': params.password},
     ));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+        email: faker.internet.email(), password: faker.internet.password());
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
