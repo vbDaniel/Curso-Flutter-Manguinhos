4735080821723af0d064a9a67a95ba3f31bcbce4
rethink-daniel <daniel.vidal@rethink.dev>
2022-07-05 14:04:35 -0300

    chore: move sut to serUp

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -14,22 +14,39 @@ class HttpAdapter {
     @required String method,
     Map body,
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await client.post(url, headers: headers);
   }
 }
 
 class ClientSpy extends Mock implements Client {}
 
 void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl(); 
+  });
   group('post', () {
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
+     
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      ));
     });
   });
 }
