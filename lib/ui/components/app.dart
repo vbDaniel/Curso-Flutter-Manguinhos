357403b7b387512ede5ff0f0bf507cf94ac16d9e
rethink-daniel <daniel.vidal@rethink.dev>
2022-08-01 11:42:47 -0300

    chore: Icon app

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/pages.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    
     final primaryColor = Color.fromRGBO(136, 14, 79, 1);
     final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
     final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
 
     return MaterialApp(
-    
       title: '4Dev',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -21,30 +23,25 @@ class App extends StatelessWidget {
         backgroundColor: Colors.white,
         textTheme: TextTheme(
           headline1: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: primaryColorDark
-            ),
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: primaryColorDark),
         ),
         inputDecorationTheme: InputDecorationTheme(
           enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: primaryColorLight)
-          ),
+              borderSide: BorderSide(color: primaryColorLight)),
           focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: primaryColorDark)
-          ),
+              borderSide: BorderSide(color: primaryColorDark)),
           alignLabelWithHint: true,
         ),
         buttonTheme: ButtonThemeData(
-          colorScheme: ColorScheme.light(primary: primaryColor),
-          buttonColor: primaryColor,
-          splashColor: primaryColorLight,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20)
-          )
-        ),
+            colorScheme: ColorScheme.light(primary: primaryColor),
+            buttonColor: primaryColor,
+            splashColor: primaryColorLight,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            textTheme: ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20))),
       ),
       home: LoginPage(),
     );
