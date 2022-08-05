5086459df652a2ce1755b2a7a060812b79c94811
rethink-daniel <daniel.vidal@rethink.dev>
2022-08-05 17:34:45 -0300

    move auth mock to a method

---
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:faker/faker.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+abstract class Validation {
+  String validate({@required String field, @required String value});
+}
+
+class LoginState {
+  String emailError;
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  StreamLoginPresenter({
+    @required this.validation,
+  });
+
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+}
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  StreamLoginPresenter sut;
+  ValidationSpy validation;
+  String email;
+
+  PostExpectation mockValidationCall(String field) => 
+  when(validation.validate(field: field == null ? anyNamed('field') : field,value: anyNamed('value')));
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
+  setUp(() {
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
+    email = faker.internet.email();
+    mockValidation();
+  });
+
+  test('Should call validation with correct email', () {
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    mockValidation(value: 'error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
+  });
+}
