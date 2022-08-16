f047d11985d9a6702a82bc1968b06d8abd5f70fb
rethink-daniel <daniel.vidal@rethink.dev>
2022-08-16 15:29:03 -0300

    chore: ensure ValidationComposite returns the first error of the correct field

--- test/validatio/validetors/validation_composite_test.dart
+++ test/validatio/validetors/validation_composite_test.dart
@@ -10,28 +10,68 @@ class ValidationComposite implements Validation {
   ValidationComposite(this.validations);
 
   String validate({@required String field, @required String value}) {
-    return null;
+    String error;
+    for (final validation in validations.where((v) => v.field == field)) {
+      error = validation.validate(value);
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+    return error;
   }
 }
 
 class FieldValidationSpy extends Mock implements FieldValidation {}
 
 void main() {
-  test('Should return null if all validations returns null or empty', () {
-    final validation1 = FieldValidationSpy();
-    when(validation1.field).thenReturn('any_field');
-    when(validation1.validate(any)).thenReturn(null);
+  ValidationComposite sut;
+  FieldValidationSpy validation1;
+  FieldValidationSpy validation2;
+  FieldValidationSpy validation3;
 
+  void mockValidation1(String error) {
+    when(validation1.validate(any)).thenReturn(error);
+  }
 
-    final validation2 = FieldValidationSpy();
+  void mockValidation2(String error) {
+    when(validation2.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation3(String error) {
+    when(validation3.validate(any)).thenReturn(error);
+  }
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    when(validation1.field).thenReturn('other_field');
+    mockValidation1(null);
+
+    validation2 = FieldValidationSpy();
     when(validation2.field).thenReturn('any_field');
-    when(validation1.validate(any)).thenReturn('');
+    mockValidation2(null);
 
+    validation3 = FieldValidationSpy();
+    when(validation3.field).thenReturn('any_field');
+    mockValidation2(null);
 
-    final sut = ValidationComposite([validation1, validation2]);
+    sut = ValidationComposite([validation1, validation2, validation3]);
+  });
+  test('Should return null if all validations returns null or empty', () {
+    mockValidation2('');
 
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
   });
+
+  test('Should return first error found', () {
+    mockValidation1('error_1');
+    mockValidation2('error_2');
+    mockValidation3('error_3');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, 'error_2');
+  });
+  
 }
