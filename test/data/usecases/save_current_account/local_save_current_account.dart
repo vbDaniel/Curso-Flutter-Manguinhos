629ee5127dd4d2e34ec9d3f431eb118437eda270
rethink-daniel <daniel.vidal@rethink.dev>
2022-08-19 18:10:10 -0300

    chore: move production code to their own files

--- test/data/usecases/save_current_account/local_save_current_account.dart
+++ test/data/usecases/save_current_account/local_save_current_account.dart
@@ -1,30 +1,12 @@
+import 'package:ForDev/data/cache/cache.dart';
+import 'package:ForDev/data/usecases/usecases.dart';
+
 import 'package:ForDev/domain/entities/entities.dart';
 import 'package:ForDev/domain/helpers/helpers.dart';
-import 'package:ForDev/domain/usecases/usecases.dart';
+
 import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
-
 import 'package:test/test.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({@required this.saveSecureCacheStorage});
-
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(
-          key: 'token', value: account.token);
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void> saveSecure({@required String key, @required String value});
-}
 
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
 }
@@ -40,6 +22,13 @@ void main() {
         saveSecureCacheStorage: saveSecureCacheStorageSpy);
     account = AccountEntity(faker.guid.guid());
   });
+
+  void mockError() {
+    when(saveSecureCacheStorageSpy.saveSecure(
+            key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
+
   test('Should call SaveSecureCacheStorage with correct values', () async {
     await sut.save(account);
 
@@ -48,9 +37,7 @@ void main() {
   });
 
   test('Should throw UnexpecteError SaveSecureCacheStorage throws', () async {
-    when(saveSecureCacheStorageSpy.saveSecure(
-            key: anyNamed('key'), value: anyNamed('value')))
-        .thenThrow(Exception());
+    mockError();
 
     final future = sut.save(account);
 
