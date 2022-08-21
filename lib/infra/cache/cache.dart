e3567e24d77407c69970196d8775bbacb0aceb4d
rethink-daniel <daniel.vidal@rethink.dev>
2022-08-21 19:41:04 -0300

    move FlutteSecureStorage to his own file

---
+++ lib/infra/cache/cache.dart
@@ -0,0 +1,1 @@
+export 'cacheLocal_Storage_adapter.dart';
--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,21 +1,12 @@
+import 'package:mockito/mockito.dart';
 import 'package:faker/faker.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ForDev/data/cache/cache.dart';
-import 'package:mockito/mockito.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:ForDev/infra/cache/cache.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
 
-  LocalStorageAdapter({@required this.secureStorage});
 
-  Future<void> saveSecure(
-      {@required String key, @required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
 
 class FlutteSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
@@ -24,16 +15,29 @@ void main() {
   LocalStorageAdapter sut;
   String key;
   String value;
+
+  void mockSaveSecureError(){
+  when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
+
   setUp(() {
     secureStorage = FlutteSecureStorageSpy();
     sut = LocalStorageAdapter(secureStorage: secureStorage);
     key = faker.lorem.word();
     value = faker.guid.guid();
   });
-  
+
   test('Should call save secure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
+  });
+
+  test('Should throw if save secure throws', () async {
+    mockSaveSecureError();
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(isA<Exception>()));
   });
 }
