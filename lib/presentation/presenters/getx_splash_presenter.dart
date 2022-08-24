31a513b8285340f4f2d4a83e971e757ad3820167
rethink-daniel <daniel.vidal@rethink.dev>
2022-08-24 17:31:24 -0300

    move GetxSplashPresenter production code to his own file

---
+++ lib/presentation/presenters/getx_splash_presenter.dart
@@ -0,0 +1,23 @@
+import 'package:meta/meta.dart';
+import 'package:get/get.dart';
+
+import '../../domain/usecases/usecases.dart';
+import '../../ui/pages/pages.dart';
+
+class GetxSplashPresenter implements SplashPresenter {
+  final LoadCurrentAccount loadCurrentAccount;
+
+  var _navigateTo = RxString();
+  Stream<String> get navigateToStream => _navigateTo.stream;
+
+  GetxSplashPresenter({@required this.loadCurrentAccount});
+
+  Future<void> checkAccount() async {
+    try {
+      final account = await loadCurrentAccount.load();
+      _navigateTo.value = account == null ? '/login' : '/surveys';
+    } catch (error) {
+      _navigateTo.value = '/login';
+    }
+  }
+}
--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,28 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:ForDev/domain/entities/account_entity.dart';
 import 'package:faker/faker.dart';
-import 'package:get/state_manager.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
+import 'package:ForDev/domain/entities/account_entity.dart';
+import 'package:ForDev/presentation/presenters/presenters.dart';
 import 'package:ForDev/domain/usecases/load_current_account.dart';
-import 'package:ForDev/ui/pages/pages.dart';
 
-class GetxSplashPresenter implements SplashPresenter {
-  final LoadCurrentAccount loadCurrentAccount;
 
-  var _navigateTo = RxString();
-  Stream<String> get navigateToStream => _navigateTo.stream;
-
-  GetxSplashPresenter({@required this.loadCurrentAccount});
-
-  Future<void> checkAccount() async {
-    final account = await loadCurrentAccount.load();
-
-    _navigateTo.value = account == null ? '/login' : '/surveys';
-  }
-}
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
@@ -30,8 +15,15 @@ void main() {
   GetxSplashPresenter sut;
   LoadCurrentAccountSpy loadCurrentAccount;
 
+  PostExpectation mockLoadCurrentAccountCall() =>
+      when(loadCurrentAccount.load());
+
   void mockLoadCurrentAccount({AccountEntity account}) {
-    when(loadCurrentAccount.load()).thenAnswer((_) async => account);
+    mockLoadCurrentAccountCall().thenAnswer((_) async => account);
+  }
+
+  void mockLoadCurrentAccountError() {
+    mockLoadCurrentAccountCall().thenThrow(Exception());
   }
 
   setUp(() {
@@ -54,6 +46,14 @@ void main() {
 
   test('Should go to login page on null result', () async {
     mockLoadCurrentAccount(account: null);
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+
+    await sut.checkAccount();
+  });
+
+  test('Should go to login page on error', () async {
+    mockLoadCurrentAccountError();
 
     sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
 
