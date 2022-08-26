5f4cdbca628f3e95010a67755890261a60d67c5c
rethink-daniel <daniel.vidal@rethink.dev>
2022-08-26 18:54:56 -0300

    chore: ensure RemoteAddAccount return  Account on 200

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -2,7 +2,6 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-
 import 'package:ForDev/domain/helpers/helpers.dart';
 import 'package:ForDev/domain/usecases/add_account.dart';
 import 'package:ForDev/data/http/http.dart';
@@ -16,56 +15,53 @@ void main() {
   String url;
   AddAccountParams params;
 
+  Map mockValidData() =>
+      {'accessToken': faker.guid.guid(), 'name': faker.person.name()};
 
-  PostExpectation mockRequest() =>
-    when(httpClient.request(
-      url: anyNamed('url'), 
-      method: anyNamed('method'), 
-      body: anyNamed('body')
-    ));
+  PostExpectation mockRequest() => when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body')));
 
+  void mockHttpData(Map data) {
+    mockRequest().thenAnswer((_) async => data);
+  }
 
   void mockHttpError(HttpError error) {
     mockRequest().thenThrow(error);
   }
-
 
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAddAccount(httpClient: httpClient, url: url);
     params = AddAccountParams(
-        name: faker.person.name(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-        passwordConfirmation: faker.internet.password(),
-      );
+      name: faker.person.name(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      passwordConfirmation: faker.internet.password(),
+    );
+    mockHttpData(mockValidData());
   });
-
 
   test('Should call HttpClient with correct values', () async {
-
     await sut.add(params);
 
-    verify(httpClient.request(
-        url: url,
-        method: 'post',
-        body: {
-          'name': params.name,
-          'email': params.email,
-          'password': params.password,
-          'passwordConfirmation': params.passwordConfirmation
-        }));
+    verify(httpClient.request(url: url, method: 'post', body: {
+      'name': params.name,
+      'email': params.email,
+      'password': params.password,
+      'passwordConfirmation': params.passwordConfirmation
+    }));
   });
 
-    test('Should throw UnexpectedError if HttpClient returns 400', () async {
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
     mockHttpError(HttpError.badRequest);
 
     final future = sut.add(params);
 
     expect(future, throwsA(DomainError.unexpected));
   });
-
 
   test('Should throw UnexpectedError if HttpClient returns 404', () async {
     mockHttpError(HttpError.notFound);
@@ -75,7 +71,6 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
-
   test('Should throw UnexpectedError if HttpClient returns 500', () async {
     mockHttpError(HttpError.serverError);
 
@@ -84,7 +79,8 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
-   test('Should throw InvalidCredentialsError if HttpClient returns 403', () async {
+  test('Should throw InvalidCredentialsError if HttpClient returns 403',
+      () async {
     mockHttpError(HttpError.forbidden);
 
     final future = sut.add(params);
@@ -92,4 +88,12 @@ void main() {
     expect(future, throwsA(DomainError.emailInUse));
   });
 
+  test('Should return an Account if HttpClient returns 200', () async {
+    final validData = mockValidData();
+    mockHttpData(validData);
+
+    final account = await sut.add(params);
+
+    expect(account.token, validData['accessToken']);
+  });
 }
